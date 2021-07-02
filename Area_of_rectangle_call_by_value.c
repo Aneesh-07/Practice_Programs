c3b9f29aadd00113fb3aaf96264ba3ea83149f9c
Aneesh-07 <85986613+Aneesh-07@users.noreply.github.com>
2021-07-02 15:25:55 +0530

    some new basic codes

---
+++ Area_of_rectangle_call_by_value.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+float R_area(float l,float b)
+{
+    float a=l*b;
+    return a;
+}
+int main()
+{
+    float length,breadth,area;
+    printf("Enter length and breadth of rectangle :");
+    scanf("%f %f",&length,&breadth);
+    area=R_area(length,breadth);
+    printf("Area = %f",area);
+
+}
---
+++ Area_of_triangle_using_both_call_methods.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+float V_area(float b,float h)
+{
+    float area=(1/2)*b*h;
+    return area;
+}
+float C_area(float *b,float *h)
+{
+    float area =(1/2)* *b * *h;
+    return area;
+}
+int main()
+{
+    int n;
+    float base,height,area;
+    printf("Enter 1 to calculate area of triangle by call by value and Enter 2 for call by refrence : ");
+    scanf("%d",&n);
+    printf("Enter base and height of the triangle : ");
+    scanf("%f %f",&base,&height);
+    switch(n)
+    {
+        case 1 : 
+        area=V_area(base,height);
+        printf("Area of triangle = %f",area);
+        break;
+        case 2 :
+        area = C_area(&base,&height);
+        printf("Area of triangle = %f",area);
+        break;
+        default :
+        printf("Invalid option");
+        break;    
+    }
+    return 0;
+}
+
