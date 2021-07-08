42a8dcb48e6678d105db1d2f1363420647e97072
Aneesh-07 <85986613+Aneesh-07@users.noreply.github.com>
2021-07-08 20:27:21 +0530

    changes

--- C&C++_programming/Address.c
+++ C&C++_programming/Address.c
@@ -1,14 +1,22 @@
 #include<stdio.h>
+void add(int *a,int *b)
+{
+    int sum;
+    sum= *a+*b;
+    printf("Sum of the no is %d\n",sum);
+}
 void swap(int *a,int *b)
 {
     int temp;
     temp =*a,*a=*b,*b=temp;
-    printf("a=%d  b=%d\n",*a,*b);
 }
 int main()
 {
-    int a=5,b=6;
-    printf("a=%d  b=%d\n",a,b);
+    int a,b;
+    printf("Enter number 1 and 2\n");
+    scanf("%d %d",&a,&b);
+    printf("The numbers are a=%d  b=%d\n",a,b);
+    add(&a,&b);
     swap(&a,&b);
-printf("a=%d  b=%d\n",a,b);
+printf("The numbers after swapping are a=%d  b=%d\n",a,b);
 }
