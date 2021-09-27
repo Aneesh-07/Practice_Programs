80602d0fa04964ad0e6cc82cd58512b3a7c65bbd
Aneesh-07 <aneeshtripathi@gmail.com>
2021-09-27 14:15:48 +0530

    codes

--- C&C++_programming/array_sub_division_CSES.c
+++ C&C++_programming/array_sub_division_CSES.c
@@ -1,14 +1,13 @@
-#include<bits/stdc++.h>
-using namespace std;
+#include<stdio.h>
+
 int main()
 {
     int n,x,c=0;
     scanf("%d %d",&n,&x);
     int a[n];
     for(int i=0;i<n;i++)
-      {scanf("%d",&a[i]);
-      }
-      sort(a,a+n);
+      scanf("%d",&a[i]);
+
     for(int i=0;i<n;i++)
     {
         int sum=0;
