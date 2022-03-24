9fed98e758bb1adac3037dd489c268e23cb61e6d
Aneesh-07 <aneeshtripathi@gmail.com>
2022-03-24 19:58:57 +0530

    chages

--- CPP_Programming/ashish_function.cpp
+++ CPP_Programming/ashish_function.cpp
@@ -1,20 +1,65 @@
-#include <bits/stdc++.h>
-using namespace std;
-void swap(int *a,int *b)
-{
-int t;
-t=*a;
-*a=*b;
-*b=t;
-}
 
-int main()
+struct ListNode
 {
-    
-    int x,y;
-    cin>>x>>y;
-    cout<<x<<" "<<y<<endl;
-   swap(&x,&y);
-    cout<<x<<" "<<y<<endl;
-    return 0;
+    int val;
+    struct ListNode *next;
+};
+
+#include <bits/stdc++.h>
+struct ListNode *addTwoNumbers(struct ListNode *list1, struct ListNode *list2)
+{
+    struct ListNode *h1 = (struct ListNode *)malloc(sizeof(struct ListNode)), *p, *l1, *l2, *x;
+    p = h1;
+    l1 = list1;
+    l2 = list2;
+    p->val = l1->val + l2->val;
+    l1 = l1->next;
+    l2 = l2->next;
+    p->next = NULL;
+    while (l1 != NULL && l2 != NULL)
+    {
+        struct ListNode *temp = (struct ListNode *)malloc(sizeof(struct ListNode));
+        temp->val = l1->val + l2->val;
+        l1 = l1->next;
+        l2 = l2->next;
+        temp->next = NULL;
+        p->next = temp;
+        p=p->next;
+    }
+    if (l1 == NULL)
+        x = l2;
+    else
+        x = l1;
+
+    while (x != NULL)
+    {
+        struct ListNode *temp = (struct ListNode *)malloc(sizeof(struct ListNode));
+        temp->val = x->val;
+        x = x->next;
+        temp->next = NULL;
+        p->next = temp;
+        p=p->next;
+    }
+
+    x = h1;
+    while (x->next != NULL)
+    {
+        if ((x->val) / 10 != 0)
+        {
+            (x->val) = (x->val) % 10;
+            ((x->next)->val)++;
+        }
+        x = x->next;
+    }
+
+    if ((x->val) / 10 != 0)
+    {
+        struct ListNode *temp = (struct ListNode *)malloc(sizeof(struct ListNode));
+        temp->val = 1;
+        (x->val) = (x->val) % 10;
+        x->next=temp;
+        temp->next=NULL;
+    }
+
+    return h1;
 }
